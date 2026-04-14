e9c303143812ff0229dda293f2d57af101eb0d00
Farrell Fileas <fileasfarrell@gmail.com>
2026-04-14 08:34:50 -0700

    add OKE and networking resources

---
+++ terraform/infra/oke.tf
@@ -0,0 +1,43 @@
+resource "oci_containerengine_cluster" "cp" {
+    compartment_id     = local.compartment_id
+    kubernetes_version = local.kubernetes_version
+    name               = "OKE-Cluster"
+    vcn_id             = oci_core_vcn.main.id
+
+    endpoint_config {
+        subnet_id = oci_core_subnet.endpoint.id
+        is_public_ip_enabled = true
+    }
+
+    type = "BASIC_CLUSTER"
+}
+
+resource "oci_containerengine_node_pool" "worker" {
+    cluster_id        = oci_containerengine_cluster.cp.id
+    compartment_id     = local.compartment_id
+    name               = "OKE-Node-Pool"
+    node_shape        = "VM.Standard.A1.Flex"
+    kubernetes_version = local.kubernetes_version
+    
+    node_source_details {
+        source_type = "IMAGE"
+        image_id   = data.oci_core_images.oke-node-image.images[0].id
+    }
+
+    node_shape_config {
+        ocpus = 2
+        memory_in_gbs = 12
+    }
+
+    node_config_details {
+        placement_configs {
+            availability_domain = data.oci_identity_availability_domains.ads.availability_domains[0].name
+            subnet_id          = oci_core_subnet.worker.id
+        }
+        placement_configs {
+            availability_domain = data.oci_identity_availability_domains.ads.availability_domains[1].name
+            subnet_id          = oci_core_subnet.worker.id
+        }
+        size = 2
+    }
+}
