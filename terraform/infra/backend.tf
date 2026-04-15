3ca9159893d45be2b53cc8cae77c8423bbd6fba4
Farrell Fileas <fileasfarrell@gmail.com>
2026-04-15 15:18:07 -0700

    state file to local for simplicity

--- terraform/infra/backend.tf
+++ terraform/infra/backend.tf
@@ -1,16 +1,6 @@
 terraform {
-    backend "s3" {
-        bucket                      = "region-health-tfstate"
-        key                         = "oke/terraform.tfstate"
-        region                      = "us-phoenix-1"
-        endpoints = {
-            s3 = "https://axhcuwsuvvsi.compat.objectstorage.us-phoenix-1.oraclecloud.com"
-        }
-        shared_credentials_file     = "~/.oci/credentials"
-        skip_region_validation      = true
-        skip_credentials_validation = true
-        skip_metadata_api_check     = true
-        force_path_style            = true
-        skip_requesting_account_id  = true
-    }
+    # Local for simplicity
+  backend "local" {
+    path = "terraform.tfstate"
+  }
 }
